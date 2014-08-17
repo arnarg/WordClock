779e05bdd4dd39cd7a95e7ef3f1ce4e19166d4f3
Arnar Gauti Ingason <granra@gmail.com>
2014-08-17 23:22:46 +0000

    Added i2c library for DS1307 RTC

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include <avr/io.h>
+#include "i2cmaster.h"
+
+// SCL on PD5, SDA on PD4
+
+#define DATA	PB2
+#define CLOCK	PB1
+#define LATCH	PB0
 
 int main(void)
 {
+	// Set shift register pins as outputs
+	DDRB = (1 << DATA) | (1 << CLOCK) | (1 << LATCH);
+
+	// Write the latch pin high
+	PORTB = (1 << LATCH);
 	while(1)
 	{
-		
+
 	}
 }
